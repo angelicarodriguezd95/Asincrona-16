ece04c4f4dcf5acfc9e4e52b7deec729b0a06a31
angelicarodriguezd95 <118750418+angelicarodriguezd95@users.noreply.github.com>
2023-01-22 21:03:16 -0500

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('d16-rutas app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('asincrona-16 app is running!');
   });
 });
