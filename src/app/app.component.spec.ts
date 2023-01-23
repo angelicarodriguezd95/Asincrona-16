48fdb7d2260c0ff64a4ab90efe76d59117a20b28
angelicarodriguezd95 <118750418+angelicarodriguezd95@users.noreply.github.com>
2023-01-22 21:00:24 -0500

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'd16-rutas'`, () => {
+  it(`should have as title 'asincrona-16'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('d16-rutas');
+    expect(app.title).toEqual('asincrona-16');
   });
 
   it('should render title', () => {
